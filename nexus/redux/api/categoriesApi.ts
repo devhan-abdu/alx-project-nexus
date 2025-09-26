3a8dcc088246e9bde5030077d2a19f5544910c48
devhan-hub <hanabduman@gmail.com>
2025-09-26 23:02:12 +0300

    fix: remove extra space in baseurl

--- nexus/redux/api/categoriesApi.ts
+++ nexus/redux/api/categoriesApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const categoryApi = createApi({
     reducerPath: 'categoryApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://10.4.111.25:5000/api/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://10.4.106.239:5000/api/" }),
     endpoints: (builder) => ({
         getCategories: builder.query<Category[], void>({
             query: () => 'categories',
