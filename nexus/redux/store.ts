8b02a397ef3a6812b335315ed48166d671866ee7
devhan-hub <hanabduman@gmail.com>
2025-09-26 15:18:21 +0300

    feat: Add RTK Query API slices to store

--- nexus/redux/store.ts
+++ nexus/redux/store.ts
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { categoryApi } from "./api/categoriesApi";
+import { productApi } from "./api/productApi";
 import cartReducer from './slices/cartSlice';
+
 export const store = configureStore({
-    reducer:{
-       cart:cartReducer,
-    }
+    reducer: {
+        cart: cartReducer,
+        [productApi.reducerPath]: productApi.reducer,
+        [categoryApi.reducerPath]: categoryApi.reducer
+    },
+
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware()
+            .concat(productApi.middleware)
+            .concat(categoryApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>
