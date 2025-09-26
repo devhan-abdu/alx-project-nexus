1ed942ae98d3d83a89d851ccbff0054109005099
devhan-hub <hanabduman@gmail.com>
2025-09-26 15:44:07 +0300

    fit: add not found page

---
+++ nexus/app/+not-found.tsx
@@ -0,0 +1,17 @@
+import { Link, Stack } from 'expo-router';
+import { View } from 'react-native';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <View className='flex-1 bg-[#25292e] items-center justify-center'>
+        <Link href="/" className='text-2xl text-white underline'>
+          Go back to Home screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+
+
