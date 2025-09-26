638e64e27c58fbb1e2d74488d0da1d9f6be2ba09
devhan-hub <hanabduman@gmail.com>
2025-09-26 15:26:40 +0300

    update interface

--- nexus/interface/index.ts
+++ nexus/interface/index.ts
@@ -1,16 +1,43 @@
-export type Category = 'Smartphones' | 'Headphones' | 'Laptops' | 'Accessories';
 
-export interface Product{
-    id: string;
-    title: string;
-    price: number;
-    rating?: number;
-    category: Category;
-    description: string;
-    image: any;
+export interface Product {
+  id: number;
+  slug: string;
+  title: string;
+  shortDesc: string;
+  image: string;
+  categoryId: number;
+  price: number;
+  stock: number;
+  rating: number;
+  numReviews: number;
+  specs: string[];
+};
+export interface Category {
+  id: number;
+  name: string;
+  image: string;
+  slug: string;
+}
+export interface Review {
+  id: number;
+  rating: number;
+  comment: string;
+  user: {
+    name: string;
+  };
+}; 
 
+export interface ProductDetail extends Product {
+  longDesc: string;
+  discount: number;
+  reviews: Review[];
 }
 
-export interface Cart extends Omit<Product, 'rating' | 'category'> {
-  quantity: number;
+export interface CartItems {
+  image: string;
+  title: string;
+  desc: string;
+  quantity:number;
+  price:number;
+  productId:number;
 }
