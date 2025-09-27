b106e0e61d93f36ed1026c5c1fa05e324ab1fea9
devhan-hub <hanabduman@gmail.com>
2025-09-27 14:48:06 +0300

    fit: add profile page

---
+++ nexus/app/(tabs)/profile.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default  function SearchScreen() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-lg text-primary">Profile screen</Text>
+        </View>
+    )
+}
